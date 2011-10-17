9bc3416a5b1781764dec6553152813e04419f82f
Даниленко Александр <hdg700@gmail.com>
2011-10-17 23:36:19 +1000

    pyinotify experiments

--- try_pyinotify.py
+++ try_pyinotify.py
@@ -7,22 +7,52 @@ import os
 import pyinotify
 from pyinotify import WatchManager, Notifier, ThreadedNotifier, ProcessEvent
 
+
 wm = WatchManager()
 
 mask = pyinotify.IN_MODIFY
 
+prjs = {}
+
 class PTmp(ProcessEvent):
     def process_IN_MODIFY(self, event):
-        print [v for v,k in wdc.items() if event.wd == k], event.name
-        print [v for v,k in wdt.items() if event.wd == k], event.name
+        class FoundException(Exception):
+            def __init__(self, project):
+                self.project = project
+
+        class FoundCodeException(FoundException):
+            pass
+
+        class FoundTestException(FoundException):
+            pass
+
+        try:
+            for p, wdct in prjs.items():
+                if event.wd in wdct[0]:
+                    raise(FoundCodeException(p))
+                elif event.wd in wdct[1]:
+                    raise(FoundTestException(p))
+
+        except FoundCodeException as e:
+            print 'Found code:', e.project, ' -> ', event.pathname
+        except FoundTestException as e:
+            print 'Found test:', e.project, ' -> ', event.pathname
+        else:
+            print 'Not found'
+        #print [v for v,k in wdc.items() if event.wd == k], event.name
+        #print [v for v,k in wdt.items() if event.wd == k], event.name
 
 notifier = Notifier(wm, PTmp())
 
-wdc = wm.add_watch('./application/modules', mask, rec=True)
-wdt = wm.add_watch('./tests', mask, rec=True)
+#wdc = wm.add_watch('./application/modules', mask, rec=True)
+#wdt = wm.add_watch('./tests', mask, rec=True)
+#prjs['spravka'] = (wm.add_watch('./application/modules', mask, rec=True), wm.add_watch('./tests', mask, rec=True))
+prjs['spravka'] = ([], [])
+prjs['spravka'][0].extend(wm.add_watch('./application/modules', mask, rec=True).values())
+prjs['spravka'][1].extend(wm.add_watch('./tests', mask, rec=True).values())
 
-print wdc.values()
-print wdt.values()
+#print wdc.values()
+#print wdt.values()
 
 while True:
     try:
